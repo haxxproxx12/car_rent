a873511a587ebec63b4f0e2afffd20e014dde433
haxxproxx12 <nafikov.ilya1@gmail.com>
2024-09-18 21:36:37 +0300

    add SQL base

--- car_rent/cars/admin.py
+++ car_rent/cars/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from cars.models import CarClasses, Cars
 
 # Register your models here.
+
+admin.site.register(CarClasses)
+admin.site.register(Cars)
--- car_rent/cars/models.py
+++ car_rent/cars/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# Модель - это и есть таблица
+
+class CarClasses(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField()
+
+class Cars(models.Model):
+    brand = models.CharField(max_length=64, unique=True)
+    model = models.CharField(max_length=64)
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    carClass = models.ForeignKey(CarClasses, on_delete=models.PROTECT)
