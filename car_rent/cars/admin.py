9f66e8f0dc6789b9cc24ed7737455a2b906195eb
Ilya Nafikov <nafikov.ilya1@gmail.com>
2024-09-23 13:20:10 +0300

    rebuild db.sqlite3, add cars, add img

--- car_rent/cars/admin.py
+++ car_rent/cars/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from cars.models import CarClasses, Cars
+from cars.models import CarClasses, Cars, CarBrands
 
 # Register your models here.
 
 admin.site.register(CarClasses)
 admin.site.register(Cars)
+admin.site.register(CarBrands)
--- car_rent/cars/models.py
+++ car_rent/cars/models.py
@@ -8,12 +8,15 @@ class CarClasses(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField()
 
+class CarBrands(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
 class Cars(models.Model):
-    brand = models.CharField(max_length=64, unique=True)
+    brand = models.ForeignKey(CarBrands, on_delete=models.PROTECT)
     model = models.CharField(max_length=64)
     year = models.IntegerField()
     image = models.ImageField(upload_to='', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     carClass = models.ForeignKey(CarClasses, on_delete=models.PROTECT)
