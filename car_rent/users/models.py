a39c29bc97a802d7d8d4df4bdbdfeace16bb0580
Ilya Nafikov <nafikov.ilya1@gmail.com>
2024-09-30 11:11:37 +0300

    добавил поля: телефон и документы в базу Users

--- car_rent/users/models.py
+++ car_rent/users/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
-    
+    phone_number = models.CharField(max_length=15, blank=True)
+    documents = models.FileField(upload_to='documents', blank=True)
