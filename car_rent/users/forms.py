98b40d8d43cf069ee0c25d5e709c1ed7b06c0f6e
haxxproxx12 <nafikov.ilya1@gmail.com>
2024-09-30 21:55:38 +0300

    add login register forms

--- car_rent/users/forms.py
+++ car_rent/users/forms.py
@@ -1,16 +1,26 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
 from django import forms
+from django_password_eye.fields import PasswordEye
 
 class UserLoginForm(AuthenticationForm):
-    class Meta:
+    username = forms.CharField(widget=forms.TextInput(attrs={'id': 'username', 'placeholder': 'Введите имя пользователя'}))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'type': 'password', 'id': 'password', 'placeholder': 'Введите пароль','autocomplete': 'off','data-toggle': 'password'}))
+
+    class Meta:      
         model = User
         fields = ('username', 'password')
-        labels = {
-            "user_name": "*Username",
-            "password": "*Password"
-        }    
-        widgets = {
-            "user_name":  forms.TextInput(attrs={'placeholder':'ex:test','autocomplete': 'off'}), 
-            "password": forms.PasswordInput(attrs={'placeholder':'********','autocomplete': 'off','data-toggle': 'password'}),
-        }
+
+
+class UserRegistrationForm(UserCreationForm):
+    model = User
+    fields = ('username', 'email', 'password', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'id': 'username', 'placeholder': 'Введите имя пользователя'}))
+
+    password = PasswordEye(widget=forms.PasswordInput(attrs={'type': 'password', 'id': 'password', 'placeholder': 'Введите пароль','autocomplete': 'off','data-toggle': 'password'}))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'type': 'password', 'id': 'password2', 'placeholder': 'Подтвердите пароль','autocomplete': 'off','data-toggle': 'password'}))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={'id': 'email', 'placeholder': 'Введите email'}))
